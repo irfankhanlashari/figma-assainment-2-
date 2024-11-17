2fa9484d8b2bc5e8890e2efcb7f72112743b1a63
irfankhanlashari <165201699+irfankhanlashari@users.noreply.github.com>
2024-11-18 00:00:26 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+
+function page() {
+  return (
+    <>
+    <div className="bg-[#252B42] w-[1440px] h-[1132px]">
+      <div className="w-[1322px] h-[91px] ml-[59px] flex gap-[25px] text-white bg-[#252B42]  border border-orange-600">
+        <div className="w-[187px] h-[58px] mt-[17px] ml-[136px]  border border-white">
+          <div className="w-[152px] h-[32px] mt-[13px]">
+            <h3 className="font-montserrat font-bold text-[24px] leading-[32px] tracking-[0.1px]">
+              BrandName
+            </h3>
+          </div>
+        </div>
+        <div className="w-[815px] h-[58px] mt-[16px] left-[364px] flex justify-between border border-green-600">
+          <div className="w-[275px] h-[24px] mt-[17px] gap-[21px] border border-orange-600">
+            <ul className="flex items-center justify-between font-montserrat">
+              <li>Home</li>
+              <li>Product</li>
+              <li>Pricing</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="w-[189px] h-auto pt-[3px] left-[626px] gap-[45px] flex justify-between items-center border border-yellow-600">
+            <button className="w-[41px] h-[22px] font-serif font-bold text-size[14px] leading-[22px] tracking-[0.2px] align-right font-montserrat">Login</button>
+            <button className="w-[110px] h-[52px] rounded-[5px] py-[15px] px-[25px] gap-[15px] text-nowrap bg-[#23A6F0] font-montserrat">JOIN US</button>
+          </div>
+        </div>
+      </div>
+      <div className="w-[1046px] h-[1028px] top-[104px] ml-[197px] py-[80px] gap-[80px] flex flex-col items-center text-white border border-green-500 ">
+        <div className="w-[699px] h-[496px] py-[40px] gap-[40px] flex flex-col items-center border border-green-500">
+          <div className="w-[77px] h-[24px] border border-black">
+            <h5 className="font-montserrat font-bold text-[16px] leading-[24px] tracking-[0.1px] text-[#23A6F0]">
+              Welcome
+            </h5>
+          </div>
+          <div className="w-[542px] h-[160px] font-sans font-bold text-[58px] leading-[80px] tracking-[0.2px] text-center border border-y-green-500 ">
+            Selling on the internet like a pro
+          </div>
+          <div className="w-[536px] h-[60px] font-montserrat font-normal text-[22px] leading-[30px] tracking-[0.2px] text-center border border-green-500">
+            We know how large objects will act, but things on a small scale just
+            do not act that way.
+          </div>
+          <div className="w-[365px] h-[52px] gap-[10px] flex items-center justify-center border border-green-500">
+            <div className="w-[193px] h-[52px] font-montserrat rounded-[5px] py-[15px] px-[40px] gap-[10px] whitespace-nowrap bg-[#23A6F0]">
+              Get Quote Now
+            </div>
+            <div className=" w-[162px] h-[52px] rounded-[5px] border border-[#23A6F0] py-[15px] px-[40px] gap-[10px] ">
+              <div className="w-[82px] h-[22px] text-[#23A6F0] font-montserrat font-bold text-[14px] leading-[22px] tracking-[0.2px] text-center">
+                Learn More
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-[1046px] h-[292px] gap-[30px] flex border border-red-600">
+          <div className="w-[328px] h-[292px] py-[35px] px-[40px] gap-[20px] flex flex-col bg-white drop-shadow border border-white">
+            <div className=" w-[70px] h-[76px] rounded-[10px] py-[22px] px-[19px] gap-[10px] bg-[#FFDCD1] border border-red-600"></div>
+            <div className="w-[135px] h-[24px] border border-red-500 ">
+              <h3 className="font-montserrat font-bold text-size-[16px] leading-[24px] tracking-[0.1px] text-[#252B42]">training Courses
+              </h3>
+              </div>
+            <div className="w-[50px] h-[2px] bg-[#E74040]"></div>
+            <div className="w-222px] h-[60px]">
+              <p className="font-montserrat font-normal text-size-[14px] leading-[20px] tracking-[0.2px] text-[#737373]">
+              The gradual accumulation of 
+              information about atomic and 
+              small-scale behaviour...
+              </p>
+            </div>
+
+          </div>
+
+          <div className="w-[328px] h-[292px] py-[35px] px-[40px] gap-[20px] flex flex-col bg-white drop-shadow border border-white">
+            <div className=" w-[70px] h-[76px] rounded-[10px] py-[22px] px-[19px] gap-[10px] bg-[#B9EAA8] border border-red-600"></div>
+            <div className="w-auto h-[24px] border border-red-500 ">
+              <h3 className="font-montserrat font-bold text-size-[16px] leading-[24px] tracking-[0.1px] text-nowrap text-[#252B42]">2,769 online courses
+              </h3>
+              </div>
+            <div className="w-[50px] h-[2px] bg-[#E74040]"></div>
+            <div className="w-222px] h-[60px]">
+              <p className="font-montserrat font-normal text-size-[14px] leading-[20px] tracking-[0.2px] text-[#737373]">
+              The gradual accumulation of 
+              information about atomic and 
+              small-scale behaviour...
+              </p>
+            </div>
+
+          </div>
+          <div className="w-[328px] h-[292px] py-[35px] px-[40px] gap-[20px] flex flex-col text-white bg-[#23A6F0] drop-shadow border border-white">
+            <div className=" w-[70px] h-[76px] rounded-[10px] py-[22px] px-[19px] gap-[10px] bg-[#FFDCD1] border border-red-600"></div>
+            <div className="w-[135px] h-[24px] border border-red-500 ">
+              <h3 className="font-montserrat font-bold  text-size-[16px] leading-[24px] tracking-[0.1px]">training Courses
+              </h3>
+              </div>
+            <div className="w-[50px] h-[2px] bg-white"></div>
+            <div className="w-222px] h-[60px]">
+              <p className="font-montserrat font-normal text-size-[14px] leading-[20px] tracking-[0.2px]">
+              The gradual accumulation of 
+              information about atomic and 
+              small-scale behaviour...
+              </p>
+            </div>
+
+          </div>
+
+
+
+
+        </div>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default page;
